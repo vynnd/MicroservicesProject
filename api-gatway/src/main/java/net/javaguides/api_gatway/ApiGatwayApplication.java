a1da56826b2eff5fb5389d8b42261fe98fffb479
vynnd <nguynhudangvy5.10.1998@gmail.com>
2024-05-24 15:38:01 +0700

    BE: API Gateway

---
+++ api-gatway/src/main/java/net/javaguides/api_gatway/ApiGatwayApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.api_gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatwayApplication.class, args);
+	}
+
+}
---
+++ api-gatway/src/test/java/net/javaguides/api_gatway/ApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.api_gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
